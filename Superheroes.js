8b891ebd97fc5d74c4464b40993eb19e8313a23c
Roqueotha04 <roqueotha04@gmail.com>
2023-10-10 10:25:18 -0300

    se pueden ver los superheroes de a 20 de forma hardcodeada, se debe agregar un boton para automatizar esto

--- Superheroes.js
+++ Superheroes.js
@@ -1,10 +1,7 @@
-const url = "https://superheroapi.com/api.php/3715170588728325/2";
-
-
-
-
- async function pedidoAPI(){
-   try {
+ async function PedidoAPI(id){
+    let url = `https://superheroapi.com/api.php/3715170588728325/${id}`; //Al no haber una url q llame a todos (o no encontre), hago cada una en el metodo especifico
+  
+    try {
     const response = await fetch(url, { ///ACA DECLARO RESPONSE
       method: "GET"
     });
@@ -16,9 +13,9 @@ const url = "https://superheroapi.com/api.php/3715170588728325/2";
    /// console.log(response);
 
     data = await response.json(); //DECLARO DATA PARA GUARDAR LOS DATOS EN JSON Y RETORNARLOS
-    console.log(data);
+   // console.log(data);
 
-   // return data;
+    return data;
 
   } catch (error) {
     console.error("Error:", error);
@@ -26,43 +23,33 @@ const url = "https://superheroapi.com/api.php/3715170588728325/2";
   }
 }
 
+///Fin llamado Api por Id
 
-function pedidoAPI2(){
-return new Promise ( (resolve,reject) =>{
-        try {
-        fetch (url,{
-            method:"GET"
-        });
-
-        
-            if(!response.ok){
-                throw new Error ("Error en el llamado");
-            }
-            const data= response.json;
-            console.log(data);
-
-        }catch(error){
-            console.error ("Error: ", error);
-        }
-    }
-    )
-}
-
-pedidoAPI();
-//pedidoAPI2();
-
-async function asyncCall() {
-    console.log('calling');
+//Inicio busqueda superheroes
+async function MostrarSuperheroes() {
+    console.log('Llamando...');
     try{ 
-        let result = await PedidoAPI();
+        let i=1;
+        SolicitarDeA20SuperHeroes(i);
 
-        console.log('respuesta JSON');
-        console.log(result); 
-
-        console.log('joke');
-        console.log(result.value);
          //agregarElementos(result.value);
     }catch(error){
       console.log(error);
     };
   }
+//Fin busqueda superheroes
+
+async function SolicitarDeA20SuperHeroes(i){
+    let j=i+20;
+    for (i;i<j;i++){
+        let result = await PedidoAPI(i); ///Llamo a pedido api, con el numero de id llama un superheroe
+
+       // console.log('respuesta JSON');
+        //console.log(result); 
+
+       // console.log('Nombre superheroe: ');
+        console.log(result.name);
+    }
+}
+
+MostrarSuperheroes();
