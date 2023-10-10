a7cbf14849373fc752369fc2a0cb60904c17ee3f
Roqueotha04 <roqueotha04@gmail.com>
2023-10-10 09:49:03 -0300

    funcion de llamado a API con fetch basica, se deben agregar modificciones para buscar heroe por ID

--- Superheroes.js
+++ Superheroes.js
@@ -1,1 +1,68 @@
-const url = "https://superheroapi.com/api.php/3715170588728325";
+const url = "https://superheroapi.com/api.php/3715170588728325/2";
+
+
+
+
+ async function pedidoAPI(){
+   try {
+    const response = await fetch(url, { ///ACA DECLARO RESPONSE
+      method: "GET"
+    });
+
+    if (!response.ok) { //RESPONSE ES EL NOMBRE DE LA CONSTANTE QUE GUARDO CON FETCH
+      throw new Error("Error en el llamado");
+    }
+
+   /// console.log(response);
+
+    data = await response.json(); //DECLARO DATA PARA GUARDAR LOS DATOS EN JSON Y RETORNARLOS
+    console.log(data);
+
+   // return data;
+
+  } catch (error) {
+    console.error("Error:", error);
+    throw error;
+  }
+}
+
+
+function pedidoAPI2(){
+return new Promise ( (resolve,reject) =>{
+        try {
+        fetch (url,{
+            method:"GET"
+        });
+
+        
+            if(!response.ok){
+                throw new Error ("Error en el llamado");
+            }
+            const data= response.json;
+            console.log(data);
+
+        }catch(error){
+            console.error ("Error: ", error);
+        }
+    }
+    )
+}
+
+pedidoAPI();
+//pedidoAPI2();
+
+async function asyncCall() {
+    console.log('calling');
+    try{ 
+        let result = await PedidoAPI();
+
+        console.log('respuesta JSON');
+        console.log(result); 
+
+        console.log('joke');
+        console.log(result.value);
+         //agregarElementos(result.value);
+    }catch(error){
+      console.log(error);
+    };
+  }
