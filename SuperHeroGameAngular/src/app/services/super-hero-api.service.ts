dae2e53babda1dfe9700f837be16fb52c832cd98
Enzo Petela <enzo03453@gmail.com>
2023-10-24 21:37:05 -0300

    Corregido parcialmente el modal, falta agregar información adicional.

--- SuperHeroGameAngular/src/app/services/super-hero-api.service.ts
+++ SuperHeroGameAngular/src/app/services/super-hero-api.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Heroe } from '../interfaces/Heroe'
 
 @Injectable({
   providedIn: 'root'
@@ -11,12 +10,12 @@ export class SuperHeroApiService {
   apiUrl: string;
   apiKey: string;
 
-  constructor(private http: HttpClient) { 
+  constructor(private http: HttpClient) {
     this.apiUrl = "https://superheroapi.com/api.php/";
     this.apiKey = "/3715170588728325/"
-   }
+  }
 
-   getListHeroes(): Observable<any> {
+  getListHeroes(): Observable<any> {
     return this.http.get<any>(`${this.apiUrl}${this.apiKey}search/a`);
   }
 
