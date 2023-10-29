1ee241ac64c21fa2d059a308cc31ca75fad55058
Tincho1612 <martinchaves2003@gmail.com>
2023-10-29 19:26:00 -0300

    FormRegister 1.0

---
+++ SuperHeroGameAngular/src/app/interfaces/User.ts
@@ -0,0 +1,14 @@
+export class User{
+    nombre:string
+    apellido:string
+    email:string
+    password:string
+    constructor(nombre:string,apellido:string,correo:string,password:string){
+        this.nombre=nombre
+        this.apellido=apellido
+        this.email=correo
+        this.password=password
+        
+    }
+    
+}
