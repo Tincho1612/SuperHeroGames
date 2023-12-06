b84e25cf89c7a5082a48c26552a1fa30a2a12cf2
Tincho1612 <martinchaves2003@gmail.com>
2023-12-06 20:57:07 -0300

    Funcion de cambiar email añadida

--- SuperHeroGameAngular/src/app/components/update-user/update-user.component.ts
+++ SuperHeroGameAngular/src/app/components/update-user/update-user.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-update-user',
@@ -6,5 +9,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./update-user.component.css']
 })
 export class UpdateUserComponent {
+  formEmail: FormGroup;
+  textoLogueo: string = "";
+  constructor(private fb: FormBuilder,
+    private toastr: ToastrService,private users_:UsersService){
+      this.formEmail = this.fb.group({
+        emailActual: ['', [Validators.required, Validators.email]],
+        emailNuevo: ['', [Validators.required, Validators.email]]
+      });
+  }
 
+  changeEmail(){
+    if (this.formEmail.valid){
+      const actual = this.formEmail.get('emailActual')?.value;
+      const nuevo = this.formEmail.get('emailNuevo')?.value;
+      this.users_.listusers.forEach(element => {
+        if (element.email==actual){
+          element.email= nuevo
+          this.users_.currentUser.email=nuevo
+          this.users_.updateUserData(this.users_.currentUser)
+          console.log(this.users_.currentUser)
+        }
+      });
+    }
+    
+  }
 }
