65f9bd698a44236d3ae27d56ebd8b9a0fdb57808
Enzo Petela <enzo03453@gmail.com>
2024-02-02 19:07:59 -0300

    feat (Correcciones en el componente inicio)

--- SuperHeroGameAngular/src/app/components/inicio/inicio.component.ts
+++ SuperHeroGameAngular/src/app/components/inicio/inicio.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -9,22 +9,53 @@ import { Router } from '@angular/router';
 export class InicioComponent {
 
   mostrarOpciones = false;
-  
+  fondoColor = '#000000'; // Color de fondo inicial
+  transitionClass = false;
+  colorText = true;
+  ocultarOpciones = false;
+  textoInicio = "Bienvenido a Super Hero Game";
+
+  // Obtén una referencia al elemento de fondo para añadir/quitar la clase
+  @ViewChild('backgroundElement') backgroundElement!: ElementRef;
+
   constructor(
     private router: Router
-  ){}
+  ) { }
 
   mostrarColumnas() {
     this.mostrarOpciones = true;
   }
 
+  cambiarOpciones() {
+    this.colorText = false;
+    this.ocultarOpciones = true;
+    this.fondoColor = '#ffffff';
+    this.aplicarTransicion();
+  }
+
   irALogin() {
-    // Redirige a la página de login
-    this.router.navigate(['/login']);
+    // Cambia el color de fondo y redirige a la página de login
+    this.cambiarOpciones();
+    this.textoInicio = "Redireccionando al inicio de sesión..."
+    setTimeout(() => {
+      this.router.navigate(['/login']);
+    }, 1000);
   }
 
   irARegistro() {
-    // Redirige a la página de registro
-    this.router.navigate(['/register']);
+    // Cambia el color de fondo y redirige a la página de registro
+    this.cambiarOpciones();
+    this.textoInicio = "Redireccionando al registro..."
+    setTimeout(() => {
+      this.router.navigate(['/register']);
+    }, 1000);
+  }
+
+  aplicarTransicion() {
+    // Agrega la clase de transición y elimínala después de 1 segundo
+    this.transitionClass = true
+    setTimeout(() => {
+      this.transitionClass = false;
+    }, 1000);
   }
 }
